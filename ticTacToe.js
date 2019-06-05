670d0051d7960370987c62263adffb2cf4483ef2
jaywiles <jay.wiles@gmail.com>
2019-06-04 20:54:03 -0500

    almost done with game

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,3 +2,41 @@
 // console.log("sanity check")
 // ** above are good ways to check and make sure JS is linked to HTML correctly
 
+let game = ['-','-','-','-','-','-','-','-','-'];
+let turn = `O`;
+
+function placeMarker(boxNumber) {
+    const box = document.getElementById('box'+boxNumber);
+    if(box.innerHTML === '-'){
+        if(turn == 'O') {
+            turn = 'X';
+            box.innerHTML = turn;
+            box.style.color = 'white';
+        } else if(turn === 'X') {
+            turn = 'O';
+            box.innerHTML = turn;
+            box.style.color = 'white';
+        }
+    }
+
+
+
+
+
+    // if(box.innerHTML == ''){
+    // one equal sign assigns
+    // two equals signs checks true/false
+    // three equals checks for type and content
+    //     box.innerHTML = playerX;
+    // } else {
+    //     box.innerHTML = playerO;
+    // }
+    // box.innerHTML = `X`;
+
+    // console.log(box);
+    // can use console.logs to check functions throughout - but then delete them
+
+    // box.innerHTML = `X`
+    // *template literal above - makes putting text on page easier in JS
+}
+
